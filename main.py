8caa7df8645f8f40da0e9788d128419921f0033e
Jeremy Borgman <borgman.jeremy@pm.me>
2023-04-07 20:38:35 -0500

    looks for adjacent files

--- main.py
+++ main.py
@@ -9,23 +9,35 @@ def returnCodeHasCaption(returnCode: int) -> bool:
     else:
         return True
 
-def checkForCaption(inputFile: str):
+def checkForAdjacentCaption(inputFile: str) -> bool:
+    name = os.path.splitext(inputFile)
+    captionFile =  "{}.vtt".format(name[0])
+    print("Caption File Exists")
+    return os.path.isfile(captionFile)
+
+def checkForEmbeddedCaption(inputFile: str) -> bool:
     subtitleCheckCommand = "ffmpeg -i {} -c copy -map 0:s:0 -frames:s 1 -f null - -v 0 -hide_banner".format(inputFile)
     process = subprocess.run(subtitleCheckCommand.split())
-    
     if returnCodeHasCaption(process.returncode):
-        print("Has Caption")
+        print("Embedded Caption Exists")
         return True
     else:
-        print("No Caption")
         return False
+
+
+def checkForCaption(inputFile: str) -> bool:
+    return checkForAdjacentCaption(inputFile) or checkForEmbeddedCaption(inputFile)
+
 
 input = "test.m4v"
 
-model = whisper.load_model("base")
-writer = get_writer("vtt", os.getcwd())
-result = model.transcribe(input)
-writer(result, input)
+if not checkForCaption(input):
+    print("Missing Caption...Generating...",end="", flush=True)
+    model = whisper.load_model("base")
+    writer = get_writer("vtt", os.getcwd())
+    result = model.transcribe(input)
+    writer(result, input)
+    print("Done")
 
 
 
