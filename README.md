3850e1023c51303ed523c6b8a8750ba483772cb9
Jeremy Borgman <borgman.jeremy@pm.me>
2023-04-08 13:08:29 -0500

    Initial Draft is Working

---
+++ README.md
@@ -0,0 +1,33 @@
+## Subtitle Adder
+
+This script will crawl a directory looking for videos. When it finds a video file it will check if the video has
+an adjacent caption file or embedded captions.  If it has neither, whisper will be used to generate a 
+.vtt caption file.
+
+## Dependencies
+
+### ffpmeg
+#### On Debian / Ubuntu
+```
+apt install ffmpeg
+```
+
+### whisper
+#### Using pip (linux)
+```
+python3 -m venv venv
+source venv/bin/activate
+python3 -m pip install openai-whisper
+```
+
+## Usage
+````
+usage: main.py [-h] [-d] path
+
+positional arguments:
+  path
+
+options:
+  -h, --help    show this help message and exit
+  -d, --dryrun  Show what files are missing captions
+```
--- main.py
+++ main.py
@@ -1,43 +1,88 @@
+"""This script adds subtitles to any video files missing them"""
+
 import subprocess
-import whisper
 import os
+import argparse
+import whisper
 from whisper.utils import get_writer
 
-def returnCodeHasCaption(returnCode: int) -> bool:
-    if returnCode == 1:
+
+def return_code_has_caption(return_code: int) -> bool:
+    """Converts the return code to a sensible bool"""
+    if return_code == 1:
         return False
     else:
         return True
 
-def checkForAdjacentCaption(inputFile: str) -> bool:
-    name = os.path.splitext(inputFile)
-    captionFile =  "{}.vtt".format(name[0])
-    print("Caption File Exists")
-    return os.path.isfile(captionFile)
 
-def checkForEmbeddedCaption(inputFile: str) -> bool:
-    subtitleCheckCommand = "ffmpeg -i {} -c copy -map 0:s:0 -frames:s 1 -f null - -v 0 -hide_banner".format(inputFile)
-    process = subprocess.run(subtitleCheckCommand.split())
-    if returnCodeHasCaption(process.returncode):
-        print("Embedded Caption Exists")
+def check_for_adjacent_caption(input_file: str) -> bool:
+    """Checks for an adjacent file that has a vtt extenion. Indicates that captions do not need to be generated"""
+    name = os.path.splitext(input_file)
+    caption_file = "{}.vtt".format(name[0])
+    return os.path.isfile(caption_file)
+
+
+def check_for_embedded_caption(input_file: str) -> bool:
+    """Checks if there are captions embedded in a video container"""
+    subtitle_check_command = f"ffmpeg -i {input_file} -c copy -map 0:s:0 -frames:s 1 -f null - -v 0 -hide_banner"
+
+    process = subprocess.run(subtitle_check_command.split())
+    if return_code_has_caption(process.returncode):
         return True
     else:
         return False
 
 
-def checkForCaption(inputFile: str) -> bool:
-    return checkForAdjacentCaption(inputFile) or checkForEmbeddedCaption(inputFile)
+def check_for_caption(input_file: str) -> bool:
+    """Check for any type of caption"""
+    return check_for_adjacent_caption(input_file) or check_for_embedded_caption(
+        input_file
+    )
 
 
-input = "test.m4v"
+def get_list_of_videos(input_dir: str) -> list[str]:
+    """Given an input directory returns a list of paths to video files"""
+    video_extensions = [".m4v", ".mp4", ".mkv"]
+    video_list = []
+    for root, _dirs, files in os.walk(input_dir):
+        for name in files:
+            ext = os.path.splitext(name)[1]
+            if ext in video_extensions:
+                video_list.append(os.path.join(root, name))
 
-if not checkForCaption(input):
-    print("Missing Caption...Generating...",end="", flush=True)
-    model = whisper.load_model("base")
-    writer = get_writer("vtt", os.getcwd())
-    result = model.transcribe(input)
-    writer(result, input)
-    print("Done")
+    return video_list
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-d",
+        "--dryrun",
+        help="Show what files are missing captions",
+        action="store_true",
+    )
+    parser.add_argument("path")
+    args = parser.parse_args()
 
+    video_list = get_list_of_videos(args.path)
+
+    if args.dryrun:
+        for video in video_list:
+            if check_for_caption(video):
+                print(f"{video}, Present")
+            else:
+                print(f"{video}, Missing")
+    else:
+        model = whisper.load_model("base")
+        for video in video_list:
+            if not check_for_caption(video):
+                print(f"{video} Missing Caption: Generating...", end="", flush=True)
+
+                output_path = os.path.dirname(video)
+                writer = get_writer("vtt", output_path)
+                result = model.transcribe(video)
+                writer(result, video)
+
+                print("Done")
+            else:
+                print(f"{video} Caption Present")
